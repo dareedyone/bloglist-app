3073c861718d0afeeaeed290c4c06149babe3799
Ridwan Ijadunola <dareedyone@gmail.com>
2020-07-22 12:48:34 +0100

    4.19: bloglist expansion, step7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,14 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const getTokenFrom = (req) => {
+	const auth = req.get("authorization");
+	const token =
+		auth && auth.toLowerCase().startsWith("bearer ") && auth.substring(7);
+	return token;
+};
 
 blogsRouter.get("/", async (request, response) => {
 	const blogs = await Blog.find({}).populate("user", {
@@ -11,8 +20,22 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-	const blog = new Blog(request.body);
+	const { title, author, url, likes } = request.body;
+	const token = getTokenFrom(request);
+	const decodedToken = token && (await jwt.verify(token, process.env.SECRET));
+	if (!decodedToken.id)
+		return response.status(401).json({ error: "token missing or invalid" });
+	const user = await User.findById(decodedToken.id);
+	const blog = new Blog({
+		title,
+		author,
+		url,
+		likes,
+		user: user._id,
+	});
 	const savedBlog = await blog.save();
+	user.blogs = user.blogs.concat(savedBlog._id);
+	await user.save();
 	response.status(201).json(savedBlog);
 });
 
